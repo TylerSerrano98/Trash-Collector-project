5249cb4402ae7f9bf25a30c777bb424e21393ce7
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-10-25 14:33:40 -0500

    added class templates to views.py employee file, as well as made the files for the corresponding html templates for those classes, i also added some url paths, i thought we might need

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,13 +1,52 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
-
+@login_required
 def index(request):
+    logged_in_user = request.user
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+
+
+def determine_day():
+
+
+
+
+
+
+
+
+
+
+def confirm_pickup(request, customer_id):
+
+
+
+
+
+
+
+def view_schedule(request, weekday):
+
+
+
+
+
+
+@login_required
+def create(request):
+
+
+
+
+@login_required
+def edit_profile(request):
