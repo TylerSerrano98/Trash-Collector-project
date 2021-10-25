c04ff0a51a9e943ee3038a8ee52aee1960be7eb0
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-10-25 14:06:15 -0500

    added some url paths to use in help building the views.py file

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -6,5 +6,8 @@ from . import views
 
 app_name = "employees"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('create/', views.create, name='create'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('<int:customer_id>/', views.confirm_pickup, name='confirm_pickup')
 ]
