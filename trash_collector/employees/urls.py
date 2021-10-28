cb35ea9da34b0b9b1b3c381156b8a464c143e098
3nigma-tik <cole.blackwood@icloud.com>
2021-10-28 13:04:52 -0700

    for pull

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('create/', views.create, name='create'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('<int:customer_id>/', views.confirm_pickup, name='confirm_pickup'),
+    path('confirm_pickup/<int:customer_id>/', views.confirm_pickup, name='confirm_pickup'),
     path('date/', views.determine_day, name="determine_day")
 ]
